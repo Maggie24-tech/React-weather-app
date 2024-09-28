61e3c878077fb2f2ae23687972b6c1e73a495a8e
Maggie24-tech <107142910+Maggie24-tech@users.noreply.github.com>
2024-09-28 09:47:51 -0600

    created structure of React weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
 
-function App() {
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Maggie24-tech"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Magdalene Nsiah
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Maggie24-tech/React-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
