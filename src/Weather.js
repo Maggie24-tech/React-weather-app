f45ac0104936a6249febd5f3c2fd607fdb063804
Maggie24-tech <107142910+Maggie24-tech@users.noreply.github.com>
2024-10-01 23:45:00 -0600

    updated daily forecast icons and units

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "9e0fb79c2f66d0cd0dcf06710976a873";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(handleResponse);
   }
 
